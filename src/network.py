7f79b3d2ad0134f6d1b0c3d9a8ec9bf6a6543aa6
Plovel <vachupa@mail.ru>
2023-05-11 19:41:55 +0300

    network fix 2

--- src/network.py
+++ src/network.py
@@ -8,21 +8,24 @@ IP_TO_CONNECT = "127.0.0.1"
 
 PORTS_R = (2000, 2001)
 PORTS_S = (3000, 3001)
-PORT_R = PORTS_R[0]
-PORT_S = PORTS_S[0]
+PORT_R = None
+PORT_S = None
 
 SOCKET_R = socket.socket()
 SOCKET_S = socket.socket()
-IS_CONNECTED = False
+PORT_R_FLAG = False
+PORT_S_FLAG = False
 
 def Disconnect(send=True):
+    global PORT_R_FLAG, PORT_S_FLAG
     if send:
-        try: SOCKET_S.send("DISCONNECT".encode())
+        try: SOCKET_S.settimeout(0.1); SOCKET_S.send("DISCONNECT".encode())
         except: pass
     globals()["IS_CONNECTED"] = False
     globals()["GAME_MODE"] = "BOT"
     SOCKET_R.close()
     SOCKET_S.close()
+    PORT_R_FLAG, PORT_S_FLAG = False, False
 
 def ExitMultiplayer(txt, menu="MAIN"):
     Disconnect(True)
@@ -30,26 +33,28 @@ def ExitMultiplayer(txt, menu="MAIN"):
     if not menu is None: SetMenu(menu)
 
 def SetUpSockets():
-    global SOCKET_R, SOCKET_S
+    global SOCKET_R, SOCKET_S, PORT_R_FLAG, PORT_S_FLAG, PORT_R, PORT_S
     SOCKET_R.close()
-    SOCKET_S.close()
     SOCKET_R = socket.socket()
+    SOCKET_R.settimeout(0.1)
+
+    SOCKET_S.close()
     SOCKET_S = socket.socket()
+    SOCKET_S.settimeout(0.1)
     try:
-        if True:
-            try: SOCKET_R.bind((HOST, PORT_R))
-            except:
-                try: globals()["PORT_R"] = PORTS_R[1]; SOCKET_R.bind((HOST, PORT_R)); 
-                except: return "Failed to set recieve socket"
-        if True:
-            try: SOCKET_S.bind((HOST, PORT_S))
-            except:
-                try: globals()["PORT_S"] = PORTS_S[1]; SOCKET_S.bind((HOST, PORT_S)); 
-                except: return "Faied to set sending socket" 
-    except: return "Unknown reason"
-    SOCKET_R.listen(1)
-    print(f"I USED {PORT_R} AS R PORT")
-    print(f"I USED {PORT_S} AS S PORT")
+        if not PORT_R_FLAG:
+            for port in PORTS_R:
+                try: SOCKET_R.bind((HOST, port))
+                except: continue
+                PORT_R = port; SOCKET_R.listen(1); print(f"I USED {PORT_R} AS R PORT"); PORT_R_FLAG = True; break
+            else: return "Failed to find free R port"
+        if not PORT_S_FLAG:
+            for port in PORTS_S:
+                try: SOCKET_S.bind((HOST, port))
+                except: continue
+                PORT_S = port; print(f"I USED {PORT_S} AS S PORT"); PORT_S_FLAG = True; break
+            else: return "Failed to find free S port"
+    except: raise Exception("how"); return "Unknown reason"
     return ""
 
 #print("I AM USING", PORT_R, "AS R AND", PORT_S, "AS S")
@@ -60,32 +65,31 @@ def EstConnection(is_opening):
     global IP_TO_CONNECT
     if is_opening:
         try:
-            SOCKET_R.settimeout(0.1)
+            SOCKET_R.settimeout(1)
             SOCKET_R, IP_TO_CONNECT = SOCKET_R.accept()
             IP_TO_CONNECT = IP_TO_CONNECT[0]
         except: return "Player did not connect"
     else:
         try:
-            try:
-                SOCKET_S.settimeout(1)
-                if PORTS_R != PORTS_R[0]: SOCKET_S.connect((IP_TO_CONNECT, PORTS_R[0]))
-                else: raise Exception("I dont want to connect to my port")
-            except:
-                SOCKET_S = socket.socket()
-                SOCKET_S.bind((HOST, PORT_S))
-                SOCKET_S.settimeout(1)
-                SOCKET_S.connect((IP_TO_CONNECT, PORTS_R[1]))
-                SOCKET_S.settimeout(10)
+            port_to_connect = PORTS_R[PORT_R == PORTS_R[0]]
+            SOCKET_S.settimeout(0.1)
+            for port in PORTS_R:
+                if PORT_R == port: continue
+                try: SOCKET_S.connect((IP_TO_CONNECT, port))
+                except: continue
+                print(PORT_R, port)
+                break
+            else: return "No avalible games"
         except: return "Failed to connect to player"
     return ""
 
 IS_OPENING_GAME = None
 timer_for_opening = time.time()
-time_out_to_recieve = 5
+time_out_to_recieve = 10
 def Connect():
     global STATE, PLAYER_COLOR, CUR_COLOR
     cur_timer = time.time() - timer_for_opening
-    if time_out_to_recieve <= cur_timer: ExitMultiplayer(["No avalible games", "No one connected"][IS_OPENING_GAME], menu="SESSIONS"); return
+    if time_out_to_recieve <= cur_timer: ExitMultiplayer(("No avalible games", "No one connected")[IS_OPENING_GAME], menu="SESSIONS"); return
     BUTTONS[1].text = str(round(time_out_to_recieve - cur_timer))
     BUTTONS[1].draw()
     pygame.display.flip()
@@ -93,21 +97,22 @@ def Connect():
     res = SetUpSockets()
     if res: ExitMultiplayer("Failed to open ports\nReason: " + res, menu=None); return
     
-    try: res += EstConnection(IS_OPENING_GAME)
-    except: return
+    res += EstConnection(IS_OPENING_GAME)
     if res: return
     
-    try: res += EstConnection(not IS_OPENING_GAME)
-    except: return
+    res += EstConnection(not IS_OPENING_GAME)
     if res: return
     
     print("Connection Established")
     globals()["IS_CONNECTED"] = True
     
     if IS_OPENING_GAME:
-        SOCKET_S.send((STATE + globals()["PLAYER_COLOR"] + CUR_COLOR).encode())
+        SOCKET_S.send((STATE + PLAYER_COLOR + CUR_COLOR).encode())
     else:
-        data = SOCKET_R.recv(100).decode()
+        try:
+            SOCKET_S.settimeout(3)
+            data = SOCKET_R.recv(100).decode()
+        except: ExitMultiplayer("Player app did not\nsend session data", menu=None); return
         STATE = data[:64]
         PLAYER_COLOR = data[64]
         ChangePlayerColor()
